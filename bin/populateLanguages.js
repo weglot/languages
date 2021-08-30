7972975cb4a897466505971e70766d7f5599f21b
Etienne Dhennin <etienne@weglot.com>
2021-08-30 12:26:36 +0200

    #22 Updated version. Refactored populateLanguages.js

--- bin/populateLanguages.js
+++ bin/populateLanguages.js
@@ -16,8 +16,6 @@ languagesJson.languages.forEach((language, i, array) => {
     fs.appendFileSync(languagesFilePath,`            'english' => "${language.english_name}",\n`);
     fs.appendFileSync(languagesFilePath,`            'local' => "${language.local_name.replace(/[""]/g, '')}",\n`);
     fs.appendFileSync(languagesFilePath,`            'rtl' => ${language.rtl},\n`);
-    fs.appendFileSync(languagesFilePath,`            'flag_path' => '${language.flag_path.replace(/[""]/g, '')}',\n`);
-    fs.appendFileSync(languagesFilePath,`            'square_flag_path' => '${language.square_flag_path.replace(/[""]/g, '')}',\n`);
     fs.appendFileSync(languagesFilePath,`        ]${i !== array.length-1 ? "," : ''}\n`);
 });
 fs.appendFileSync(languagesFilePath, "    ];\n}");
@@ -35,8 +33,6 @@ fs.appendFileSync(
 CountriesJson.countries.forEach((country, i, array) => {
     fs.appendFileSync(countriesFilePath,`        '${country.name}' => [\n`);
     fs.appendFileSync(countriesFilePath,`            'code' => '${country.code}',\n`);
-    fs.appendFileSync(countriesFilePath,`            'flag_path' => '${country.flag_path.replace(/[""]/g, '')}',\n`);
-    fs.appendFileSync(countriesFilePath,`            'square_flag_path' => '${country.square_flag_path.replace(/[""]/g, '')}',\n`);
     fs.appendFileSync(countriesFilePath,`        ]${i !== array.length-1 ? "," : ''}\n`);
 });
 fs.appendFileSync(countriesFilePath, "    ];\n}");
