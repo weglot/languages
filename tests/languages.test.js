bbb9d663cc792ac095431e4665f3205197acf486
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-07-19 17:43:17 +0200

    Add test about country flag

--- tests/languages.test.js
+++ tests/languages.test.js
@@ -1,8 +1,18 @@
 const { languages } = require("../data/languages.json");
+const fs = require("fs");
 
 test('There is no duplicate code', () => {
     const codes = languages.map((language) => language.code);
     const uniqueCodes = [...new Set(codes)];
 
     expect(codes.length).toBe(uniqueCodes.length);
+});
+
+test('All flag codes exist', () => {
+    const countryFlagCodes = languages.map((language) => language.country_flag_code);
+
+    (new Set(countryFlagCodes)).forEach(function (countryFlagCode) {
+        expect(fs.existsSync(`${__dirname}/../images/1x1/${countryFlagCode}.svg`)).toBe(true);
+        expect(fs.existsSync(`${__dirname}/../images/4x3/${countryFlagCode}.svg`)).toBe(true);
+    })
 });
