06ead2ba62ee55c1d6881374ebdbc5c2cf14ee71
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-02-15 15:19:57 +0100

    Add Kurdish Sorani and Faroese

--- tests/countries.test.js
+++ tests/countries.test.js
@@ -1,5 +1,12 @@
 const { countries } = require("../data/countries.json");
 
+test('Data structure is valid', () => {
+    countries.forEach(function (country) {
+        expect(typeof country.name).toBe('string');
+        expect(typeof country.code).toBe('string');
+    });
+});
+
 test('There is no duplicate code', () => {
     const codes = countries.map((country) => country.code);
     const uniqueCodes = [...new Set(codes)];
--- tests/languages.test.js
+++ tests/languages.test.js
@@ -1,6 +1,20 @@
 const { languages } = require("../data/languages.json");
 const fs = require("fs");
 
+test('Data structure is valid', () => {
+    languages.forEach(function (language) {
+        expect(typeof language.code).toBe('string');
+        expect(typeof language.english_name).toBe('string');
+        expect(typeof language.local_name).toBe('string');
+        expect(typeof language.rtl).toBe('string');
+        expect(typeof language.country_flag_code).toBe('string');
+        expect(typeof language.variant).toBe('string');
+
+        expect(['false', 'true']).toContain(language.rtl);
+        expect(['false', 'true']).toContain(language.variant);
+    });
+});
+
 test('There is no duplicate code', () => {
     const codes = languages.map((language) => language.code);
     const uniqueCodes = [...new Set(codes)];
