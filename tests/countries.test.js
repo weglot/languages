c927814757919f507dc32ca17712aeb0f836737f
Nicolas de C <nico2che@users.noreply.github.com>
2025-09-04 14:15:21 +0200

    Test country flags (#90)

--- tests/countries.test.js
+++ tests/countries.test.js
@@ -1,4 +1,5 @@
 const { countries } = require("../data/countries.json");
+const fs = require("fs");
 
 test('Data structure is valid', () => {
     countries.forEach(function (country) {
@@ -12,4 +13,13 @@ test('There is no duplicate code', () => {
     const uniqueCodes = [...new Set(codes)];
 
     expect(codes.length).toBe(uniqueCodes.length);
+});
+
+test('All flag codes exist', () => {
+    const countryFlagCodes = countries.map((country) => country.code);
+
+    (new Set(countryFlagCodes)).forEach(function (countryFlagCode) {
+        expect(fs.existsSync(`${__dirname}/../images/1x1/${countryFlagCode}.svg`)).toBe(true);
+        expect(fs.existsSync(`${__dirname}/../images/4x3/${countryFlagCode}.svg`)).toBe(true);
+    })
 });
