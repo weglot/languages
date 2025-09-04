0aed504fb5ff67ff62927b06d34cd2f215580265
Nicolas de C <nico2che@users.noreply.github.com>
2025-09-04 10:52:04 +0200

    Restores deleted flags (#89)

--- bin/copySvg.js
+++ bin/copySvg.js
@@ -1,22 +1,22 @@
 const fs = require("fs");
 
+const { countries } = require("../data/countries.json");
 const { languages } = require("../data/languages.json");
 
 const modulePath = `${__dirname}/../node_modules/flag-icons/flags`;
 const imagesPath = `${__dirname}/../images`;
 
 for (const size of ["1x1", "4x3"]) {
-  for (const language of languages) {
-    const sourceFlag = `${modulePath}/${size}/${language.country_flag_code}.svg`;
-    const outputFlag = `${imagesPath}/${size}/${language.country_flag_code}.svg`;
+  for (const data of [...languages, ...countries]) {
+    const code = data.country_flag_code || data.code;
+    const sourceFlag = `${modulePath}/${size}/${code}.svg`;
+    const outputFlag = `${imagesPath}/${size}/${code}.svg`;
     if (fs.existsSync(sourceFlag)) {
       fs.copyFileSync(sourceFlag, outputFlag);
       continue;
     }
     if (!fs.existsSync(outputFlag)) {
-      throw new Error(
-        `Flag ${language.country_flag_code} not found in ${sourceFlag}`
-      );
+      throw new Error(`Flag ${code} not found in ${sourceFlag}`);
     }
   }
 }
